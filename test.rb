ed0a27615242843b34676ba0ea05a6a5bf7b069f
yuichi oyama <yuichi19991208@gmail.com>
2020-09-14 21:54:15 +0900

    web_tech

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+# この一行を追記
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+# 以下課題のための追記
+# server.mount('/', WEBrick::HTTPServlet::CGIHandler, 'web.html.erb')
+server.start
